fb9a73d181d0f9604008c231ff4a965e5a72324e
Niel Archer <niel.archer@gmail.com>
2021-09-29 05:58:52 +0100

    Strip out DC's original gameobject code and add most of the UIElement equivalent.

--- Assets/Scripts/UI/ModItem.cs
+++ Assets/Scripts/UI/ModItem.cs
@@ -1,336 +1,68 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 namespace UI
 {
-	public class ModItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+	public class ModItem : VisualElement
 	{
-		[Serializable]
-		public class ModDef
+		public ModItem()
 		{
-			public int _id;
-			public int _modId;
-			public string _creator;
-			public string _title;
-			public string _desc;
-			public string _version;
-			public string _url;
-			public string _deps;
-			public int _icon;
-			public int _log;
-			public int _clean;
-			public string _folder;
-			public string _file;
-			public bool _installed;
-			public bool _isdep;
-			public bool _enabled;
+			AddToClassList("list-item");
+
+			var modItemTemplate = Resources.Load<VisualTreeAsset>("UI/ModItem");
+			Add(modItemTemplate.Instantiate());
 		}
 
-		public ModDef moddef;
-		public GameObject button_install;
-		public GameObject button_update;
-		public GameObject button_remove;
-		public GameObject button_disable;
-		public Text _status;
-		public Text _version;
-		public Text _desc;
-		private ModManager modmanager;
-
-		private void Start()
+		public void BindEntry(Mod entry)
 		{
-			modmanager = GameObject.Find("MODManagerCanvas").GetComponent<ModManager>();
-			rect = new Rect(Screen.width / 2 - 300, Screen.height / 2 - 100, 600, 25);
-			foreach (Transform eachChild in transform)
+			Debug.Log("ModItem: BindItem entered!");
+			if (entry.title != "NoModsFound")
 			{
-				if (eachChild.name == "Creator")
-				{
-					// _url = ModManager.mods[_modId].url;
-				}
+				var title     = this.Q<Label>("Name");
+				var desc      = this.Q<Label>("Description");
+				var installed = this.Q<Label>("Installed");
+				var latest    = this.Q<Label>("Latest");
+				var enable    = this.Q<Button>("Enabled");
+				var update    = this.Q<Button>("Update");
+				var remove    = this.Q<Button>("Remove");
 
-				if (eachChild.name == "Title")
-				{
-					eachChild.GetComponent<Text>().text = moddef._title;
-				}
+				title.text     = entry.title;
+				desc.text      = entry.desc;
+				installed.text = entry.version;
+				latest.text    = "";
 
-				if (eachChild.name == "Desc")
+				if (entry.enabled)
 				{
-					eachChild.GetComponent<Text>().text = moddef._desc;
+					enable.text = "Disable";
 				}
-
-				if (eachChild.name == "Version")
-				{
-					eachChild.GetComponent<Text>().text = moddef._version;
-				}
-
-				if (eachChild.name == "Icon")
-				{
-					//_icon = ModManager.mods[_modId].icon;
-				}
-
-				if (eachChild.name == "Deps")
-				{
-					// _url = ModManager.mods[_modId].url;
-				}
-
-				if (eachChild.name == "Url")
-				{
-					// _url = ModManager.mods[_modId].url;
-				}
-
-				if (eachChild.name == "Changelog")
-				{
-					// _url = ModManager.mods[_modId].url;
-				}
-
-				if (eachChild.name == "CleanInstall")
-				{
-					// _url = ModManager.mods[_modId].url;
-				}
-			}
-
-			if (moddef._installed)
-			{
-				button_install.SetActive(false);
-				button_disable.SetActive(true);
-				//check if version match before showing update button
-				button_update.SetActive(true);
-				button_remove.SetActive(true);
+				//enable.RegisterCallback<ClickEvent>(ev => ClickedEnabled());
+				//update.RegisterCallback();
+				//remove.RegisterCallback();
 			}
 			else
 			{
-				_status.text = moddef._creator;
-			}
-		}
+				var container = this.Q<VisualElement>("TextContent");
 
-
-		private List<string> alltext = new List<string>();
-		private int[] allprogress = new int[20];
-		//private bool completed2 = false;
-		private bool completed = false;
-		private string[] message = new string[20];
-		private Rect rect;
-		private static bool _inUpdate = false;
-		private bool doOnce = false;
-
-		public void btnInstall()
-		{
-			modmanager.ReloadInstalledMods();
-
-//			modmanager.panelInstall.SetActive(true);
-			modmanager.downloadStack = new DownloadStack();
-			if (moddef._deps.Length > 1)
-			{
-				if (moddef._deps.Contains(";"))
+				foreach (var child in container.Children())
 				{
-					string[] splitDeps = moddef._deps.Split(';');
-					for (int i = 0; i < splitDeps.Length; i++)
-					{
-						for (int x = 0; x < modmanager.deps.Length; x++)
-						{
-							string[] splitDepName = moddef._deps.Split('/');
-							if (modmanager.deps[x].url == splitDepName[splitDepName.Length - 1])
-							{
-								modmanager.downloadStack.deps.Add(splitDeps[i]);
-								modmanager.downloadStack.mods.Add(modmanager.deps[x]);
-								Debug.Log("Found dependencies, adding: " + modmanager.deps[x].title);
-							}
-						}
-						//modmanager.downloadStack.depsId.Add(moddef._modId);
-					}
+					container.Remove(child);
 				}
-				else
-				{
-					for (int x = 0; x < modmanager.deps.Length; x++)
-					{
-						string[] splitDepName = moddef._deps.Split('/');
-						if (modmanager.deps[x].url == splitDepName[splitDepName.Length - 1])
-						{
-							modmanager.downloadStack.deps.Add(moddef._deps);
-							modmanager.downloadStack.mods.Add(modmanager.deps[x]);
-							Debug.Log("Found a single dependency, adding: " + modmanager.deps[x].title);
-						}
-					}
-					//modmanager.downloadStack.depsId.Add(moddef._modId);
 
-				}
-			}
-			if (moddef._isdep)
-			{
-				modmanager.downloadStack.deps.Add(moddef._url);
-				Debug.Log("Button click: " + moddef._modId);
-				modmanager.downloadStack.mods.Add(modmanager.deps[moddef._modId - 1]);
-			}
-			else
-			{
-				modmanager.downloadStack.deps.Add(moddef._url);
-				Debug.Log("Button click: " + moddef._modId);
-				modmanager.downloadStack.mods.Add(modmanager.mods[moddef._modId - 1]);
-			}
-
-			//modmanager.downloadStack.depsId.Add(moddef._modId);
-			completed = false;
-			Debug.Log("Total file to download: " + modmanager.downloadStack.deps.Count);
-			modmanager.downloadStack.totalphase = modmanager.downloadStack.deps.Count; // * 2 = download + install
-			modmanager.ActiveRoutine = StartCoroutine(ModManager.GetModZip(modmanager.downloadStack, modmanager.installedMods, value => completed = value));
-			modmanager.downloading = true;
-			Debug.Log("Total phase: " + modmanager.downloadStack.totalphase);
-		}
-
-		void Update()
-		{
-			if (modmanager.downloading)
-			{
-				if (completed == true)
-				{
-					//modmanager.finishedInstall.interactable = true;
-					if (doOnce)
-					{
-						doOnce = false;
-						modmanager.PopulateModsList();
-					}
-				}
-			}
-		}
-		void OnGUI()
-		{
-			if (modmanager.downloading == true)
-			{
-				if (_inUpdate == true)
-				{
-					if (completed)
-					{
-						GUI.Label(rect, "Updating " + moddef._title + "... Done!");
-					}
-					else if (!completed)
-					{
-						GUI.Label(rect, "Updating " + moddef._title + "...");
-					}
-				}
-				if (_inUpdate == false)
-				{
-					for (int x = 0; x < modmanager.downloadStack.dlphase + 1; x++)
-					{
-						rect.y = (Screen.height / 2) - 100 + (x * 20);
-						GUI.Label(rect, modmanager.downloadStack.message[x]);
-					}
-				}
+				var emptyTemplate = new Label
+									{
+										name = "EmptyItem",
+										text = "Currently there are no mods installed.&#10;Click the &apos;Available mods&apos; button to see a list of those available."
+									};
+				emptyTemplate.AddToClassList("empty-list");
+				container.Add(emptyTemplate);
 			}
 		}
 
-		public void btnDisableEnable()
+		private void ClickedEnabled()
 		{
-			if (button_disable.GetComponentInChildren<Text>().text == "Disable")
-			{
-				//move the lua file to hold
-				File.Move(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/Lua/" + moddef._file, Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/SavedMods/disabled/" + moddef._file);
-				for (int x = 0; x < modmanager.installedMods.Length; x++)
-				{
-					if (modmanager.installedMods[x].file == moddef._file)
-					{
-						modmanager.installedMods[x].enabled = false;
-					}
-				}
-				button_disable.GetComponentInChildren<Text>().text = "Enable";
-			}
-			else if (button_disable.GetComponentInChildren<Text>().text == "Enable")
-			{
-				//move the lua file back
-				File.Move(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/SavedMods/disabled/" + moddef._file, Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/Lua/" + moddef._file);
-				//change a single variable in the installedmod.cfg to true or false for enable
-				for (int x = 0; x < modmanager.installedMods.Length; x++)
-				{
-					if (modmanager.installedMods[x].file == moddef._file)
-					{
-						modmanager.installedMods[x].enabled = true;
-					}
-				}
-				button_disable.GetComponentInChildren<Text>().text = "Disable";
-			}
-			modmanager.SaveInstalledMods();
-		}
-
-		/*installedMods = JsonHelper.FromJson<InstalledMod>(@configText);
-
-		Mod[] myArray = downloadStack.mods.ToArray();
-		File.WriteAllText(_dataPath + @"/SavedMods/InstalledMods.cfg", JsonHelper.ToJson(myArray));*/
-
-		public void btnUpdate()
-		{
-			_inUpdate = true;
-			//this udpate only the current mod select for update, not dependencies
-			button_update.GetComponent<Button>().interactable = false;
-//			modmanager.panelInstall.SetActive(true);
-			completed = false;
-			doOnce = true;
-			modmanager.downloading = true;
-			modmanager.ActiveRoutine = StartCoroutine(ModManager.UpdateMod(moddef._modId, modmanager.installedMods, value => completed = value));
-		}
-
-		public void btnRemove()
-		{
-			if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/SavedMods/backup/" + moddef._url)) { File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/SavedMods/backup/" + moddef._url); }
-			if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/SavedMods/disabled/" + moddef._file)) { File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/SavedMods/disabled/" + moddef._file); }
-			if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/Lua/" + moddef._file)) { File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/Lua/" + moddef._file); }
-			if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/Lua/" + moddef._folder)) { Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/Lua/" + moddef._folder, true); }
-
-			List<Mod> newmodlist = new List<Mod>();
-			for (int x = 0; x < modmanager.installedMods.Length; x++)
-			{
-				if (moddef._title == modmanager.installedMods[x].title) { continue; }
-				Mod test = new Mod();
-				test.creator   = modmanager.installedMods[x].creator;
-				test.title     = modmanager.installedMods[x].title;
-				test.desc      = modmanager.installedMods[x].desc;
-				test.version   = modmanager.installedMods[x].version;
-				test.deps      = modmanager.installedMods[x].deps;
-				test.isdep     = modmanager.installedMods[x].isdep;
-				test.icon      = modmanager.installedMods[x].icon;
-				test.log       = modmanager.installedMods[x].log;
-				test.folder    = modmanager.installedMods[x].folder;
-				test.file      = modmanager.installedMods[x].file;
-				test.backupzip = modmanager.installedMods[x].backupzip;
-				test.enabled   = modmanager.installedMods[x].enabled;
-				test.id        = modmanager.installedMods[x].id;
-				newmodlist.Add(test);
-			}
-			Mod[] myArray = newmodlist.ToArray();
-
-			File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/Portalarium/Shroud of the Avatar/SavedMods/InstalledMods.cfg", JsonHelper.ToJson(myArray));
-			//modmanager.SaveInstalledMods();
-			//downloadStack.mods.Remove(downloadStack.mods[y]);
-			modmanager.PopulateModsList();
-			Destroy(this.gameObject);
-		}
-
-		public void OnPointerExit(PointerEventData eventData)
-		{
-
-			if (moddef._isdep)
-			{
-				if (moddef._id % 2 == 0) this.gameObject.transform.Find("ColorBG").GetComponent<Image>().color = new Color(0f, 0.6f, 1f, 0.2f);
-				if (moddef._id % 2 == 1) this.gameObject.transform.Find("ColorBG").GetComponent<Image>().color = new Color(0f, 0.6f, 1f, 0.1f);
-			}
-			else
-			{
-				if (moddef._id % 2 == 0) this.gameObject.transform.Find("ColorBG").GetComponent<Image>().color = new Color(1f, 1f, 1f, 0.2f);
-				if (moddef._id % 2 == 1) this.gameObject.transform.Find("ColorBG").GetComponent<Image>().color = new Color(1f, 1f, 1f, 0.1f);
-			}
-		}
-
-		public void OnPointerEnter(PointerEventData eventData)
-		{
-			if (moddef._isdep)
-			{
-				this.gameObject.transform.Find("ColorBG").GetComponent<Image>().color = new Color(0f, 0.6f, 1f, 0.3f);
-			}
-			else
-			{
-				this.gameObject.transform.Find("ColorBG").GetComponent<Image>().color = new Color(1f, 1f, 1f, 0.3f);
-			}
+			// is variable 'ev' passed in by the callback?
+			// TODO handle enabling or disabling as needed
+			//Debug.Log("ev is: " + evt);
 		}
 	}
 }
